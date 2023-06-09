cf4273891d7b07716639396d259fcd160ab64c91
assim fayas <asim1fayas@gmail.com>
2023-06-09 20:05:42 +0530

    added filtering ,used fetch to make http request to json-server

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -51,9 +51,11 @@ export class DetailsComponent {
   });
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
-
+   this.housingService.getHousingLocationById(housingLocationId).then(housingLocation=>{
+    this.housingLocation=housingLocation;
+   });
   }
+  
   submitApplication(){
     this.housingService.submitApplication(
       this.applyForm.value.firstName ?? '',
